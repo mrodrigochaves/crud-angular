94cfcf6a8a32605040ebed906750b9cdf4fc1185
Márcio Rodrigo <dev.marciorodrigo@gmail.com>
2023-03-27 21:27:58 -0300

    update cars module and component

--- src/app/cars/cars.module.ts
+++ src/app/cars/cars.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import {MatTableModule} from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatTableModule } from '@angular/material/table';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { CarsRoutingModule } from './cars-routing.module';
 import { CarsComponent } from './cars/cars.component';
@@ -12,7 +14,9 @@ import { CarsComponent } from './cars/cars.component';
   imports: [
     CommonModule,
     CarsRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class CarsModule { }
